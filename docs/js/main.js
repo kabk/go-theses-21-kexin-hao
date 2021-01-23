e76f155c7df2b536c3f6cbdd59939f886e16a4ee
Kexin <kexinhao01111993@gmail.com>
2021-01-23 18:56:18 +0100

    ya.

---
+++ docs/js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  $(function(){
+    $("#abstract").click(function(){
+      location.href = "abstract.html";
+    });
+    $("#c0").click(function(){
+      location.href = "c0.html";
+    });
+    $("#c1").click(function(){
+      location.href = "c1.html";
+    });
+    $("#c2").click(function(){
+      location.href = "c2.html";
+    });
+    $("#c3").click(function(){
+      location.href = "c3.html";
+    });
+    $("#c4").click(function(){
+      location.href = "c4.html";
+    });
+    $("#c5").click(function(){
+      location.href = "c5.html";
+    });
+    $("#c6").click(function(){
+      location.href = "c6.html";
+    });
+    $("#c7").click(function(){
+      location.href = "c7.html";
+    });
+    $("#bibliography").click(function(){
+      location.href = "b.html";
+    });
+  });
+  $('.lyricsreveal').click(function(){
+      $('#lyrics' + $(this).attr('id')).toggle();
+     });
+
+
+   $('.topbar').click(function(){
+   $('#navbar').toggle();
+     // window.location.hash = target;
+   });
+
+});
